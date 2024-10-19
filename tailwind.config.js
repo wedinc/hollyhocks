f24a8ca1951e055f16bbc6bb35bc993f4f66e747
necoiro <neco22roh@gmail.com>
2024-10-19 05:41:16 +0000

    コメント

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     colors: {
-      wed: '#D2D2C6',
-      'wed-dark': '#6D695D',
+      wed: "#D2D2C6",
+      "wed-dark": "#6D695D",
+      aliceblue: "#f0f8ff",
+      royalblue: "#4169e1",
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
