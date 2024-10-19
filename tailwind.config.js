1f92fece7f5ccfae6be279d30c0bb98f9920215d
kaigetu <132892515+kaigetu@users.noreply.github.com>
2024-10-19 05:41:47 +0000

    コメント

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     colors: {
-      wed: '#D2D2C6',
-      'wed-dark': '#6D695D',
+      wed: "#D2D2C6",
+      "wed-dark": "#6D695D",
+      black: "#000000",
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
