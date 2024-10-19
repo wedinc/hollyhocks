13cda3561d9ca39958e02f4d3d919b74b209f87f
Tsurugi <tsuru9ta9@gmail.com>
2024-10-19 05:41:19 +0000

    コメント

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [count = 0, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header bg-wed">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-wed-dark">
-          Welcome to <code>WED</code> Community.
+      <header className="App-header bg-burlywood">
+        <img
+          src="https://images.photowall.com/products/48547/snowboard-jump-from-ramp.jpg?h=699&q=85"
+          className="App-logo"
+          alt="logo"
+        />
+        <p className="text-maroon">
+          <code>WED</code> Communityにようこそ！
         </p>
         <a
-          className="text-wed-dark underline"
+          className="text-maroon text-4xl underline"
           href="https://wed.company/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn WED
+          WEDについて
         </a>
+        <p className="text-4xl">{count}</p>
+        <p
+          className="text-4xl"
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          👍
+        </p>
+
+        <p
+          className="text-4xl"
+          onClick={() => {
+            setCount(count - 1);
+          }}
+        >
+          👎
+        </p>
       </header>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     colors: {
-      wed: '#D2D2C6',
-      'wed-dark': '#6D695D',
+      wed: "#D2D2C6",
+      "wed-dark": "#6D695D",
+      "burlywood": "#deb887",
+      "maroon":"#800000"
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
