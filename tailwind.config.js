e42df1a889bf8b8283863c379f58da22f60671e2
asaiharuhiko <0120mmasai@gmail.com>
2024-10-19 05:41:04 +0000

    コメント

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     colors: {
-      wed: '#D2D2C6',
-      'wed-dark': '#6D695D',
+      wed: "#D2D2C6",
+      "wed-dark": "#6D695D",
+      "wed-white": "#ffffff",
+      "wed-ugry": "#006400",
+      "black": "#252525",
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
