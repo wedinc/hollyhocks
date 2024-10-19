1844f146e0214d6cf48d3fd8c1e90491b90593ed
mori <hirom.050528@gmail.com>
2024-10-19 05:41:23 +0000

    コメント

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
     colors: {
-      wed: '#D2D2C6',
-      'wed-dark': '#6D695D',
+      wed: "#D2D2C6",
+      "wed-dark": "#6D695D",
+      "pastel-blue": "#dbedff",
+      "pastel-red": "#ff8484",
+      "pastel-purple": "#9966ff",
+      "pastel-aliceblue": "#f0f8ff",
+      "pastel-green": "#008000",
     },
-    extend: {
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
